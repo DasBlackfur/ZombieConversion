8c42dafa7308699f511d7687e6df0d9bb2c4121b
blackfur <64478051+TheBlackfurGuy@users.noreply.github.com>
2022-11-22 16:56:42 +0100

    Initial commit

---
+++ src/main/java/me/blackfur/zombieconversion/Zombieconversion.java
@@ -0,0 +1,10 @@
+package me.blackfur.zombieconversion;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Zombieconversion implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/me/blackfur/zombieconversion/mixin/ZombieConversionMixin.java
@@ -0,0 +1,16 @@
+package me.blackfur.zombieconversion.mixin;
+
+import net.minecraft.entity.mob.ZombieEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.Difficulty;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ZombieEntity.class)
+public class ZombieConversionMixin {
+    @Redirect(method = "onKilledOther(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/entity/LivingEntity;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;getDifficulty()Lnet/minecraft/world/Difficulty;"))
+    private Difficulty injected(ServerWorld serverWorld) {
+        return Difficulty.HARD;
+    }
+}
