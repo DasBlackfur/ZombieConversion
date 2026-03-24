c89086d330c45e3e37c78656f457d619fbaf962c
Lolo <lowgau@gmail.com>
2026-03-24 18:28:48 +0100

    Migrated to Mojang's official mappings

--- src/main/java/me/blackfur/zombieconversion/mixin/ZombieConversionMixin.java
+++ src/main/java/me/blackfur/zombieconversion/mixin/ZombieConversionMixin.java
@@ -1,20 +1,20 @@
 package me.blackfur.zombieconversion.mixin;
 
 import me.blackfur.zombieconversion.ConversionSetting;
-import net.minecraft.entity.mob.ZombieEntity;
-import net.minecraft.server.world.ServerWorld;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.Difficulty;
+import net.minecraft.world.entity.monster.zombie.Zombie;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import static me.blackfur.zombieconversion.ZombieConversion.GUARANTEED_CONVERSION;
 
-@Mixin(ZombieEntity.class)
+@Mixin(Zombie.class)
 public class ZombieConversionMixin {
-    @Redirect(method = "onKilledOther", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;getDifficulty()Lnet/minecraft/world/Difficulty;"))
-    private Difficulty injected(ServerWorld serverWorld) {
-        var gameRuleSetting = serverWorld.getGameRules().getValue(GUARANTEED_CONVERSION);
+    @Redirect(method = "killedEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;getDifficulty()Lnet/minecraft/world/Difficulty;"))
+    private Difficulty injected(ServerLevel serverWorld) {
+        var gameRuleSetting = serverWorld.getGameRules().get(GUARANTEED_CONVERSION);
         if (gameRuleSetting == ConversionSetting.ALWAYS) {
             return Difficulty.HARD;
         } else if (gameRuleSetting == ConversionSetting.NEVER) {
